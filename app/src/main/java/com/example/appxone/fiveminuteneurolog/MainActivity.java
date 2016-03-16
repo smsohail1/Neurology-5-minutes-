2e989a5f27203542c305b40e2cc87cbdcd130081
Muhammad sohail <syedmuhammadsohail@yahoo.com>
2016-03-16 12:08:31 +0500

    version 1.2

--- app/src/main/java/com/example/appxone/fiveminuteneurolog/MainActivity.java
+++ app/src/main/java/com/example/appxone/fiveminuteneurolog/MainActivity.java
@@ -19,29 +19,40 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnRender = (View)findViewById(R.id.btn_render);
+       // btnRender = (View)findViewById(R.id.btn_render);
         container = (LinearLayout)findViewById(R.id.fragment_layout);
 
+        container.setVisibility(View.VISIBLE);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment_layout, new PDFRenderFragment());
+        ft.commit();
+
+        //gone button after all
+     //   btnRender.setVisibility(View.GONE);
+
+
+
         //set event handling for button
-        btnRender.setOnClickListener(onClickListener());
+      //  btnRender.setOnClickListener(onClickListener());
 
     }
-    private View.OnClickListener onClickListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //replace fragment when clicked
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.fragment_layout, new PDFRenderFragment());
-                ft.commit();
-
-                //gone button after all
-                btnRender.setVisibility(View.GONE);
-                container.setVisibility(View.VISIBLE);
-            }
-        };
-    }
+//    private View.OnClickListener onClickListener() {
+//        return new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //replace fragment when clicked
+////                FragmentManager fm = getSupportFragmentManager();
+////                FragmentTransaction ft = fm.beginTransaction();
+////                ft.replace(R.id.fragment_layout, new PDFRenderFragment());
+////                ft.commit();
+////
+////                //gone button after all
+////                btnRender.setVisibility(View.GONE);
+////                container.setVisibility(View.VISIBLE);
+//            }
+//        };
+//    }
 
 }
 
--- app/src/main/java/com/example/appxone/fiveminuteneurolog/PDFRenderFragment.java
+++ app/src/main/java/com/example/appxone/fiveminuteneurolog/PDFRenderFragment.java
@@ -1,20 +1,32 @@
 package com.example.appxone.fiveminuteneurolog;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ZoomControls;
+
+import android.content.IntentFilter;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 import java.io.IOException;
 
@@ -32,6 +44,24 @@ public class PDFRenderFragment extends Fragment {
 
     private ZoomControls zoom;
 
+
+    //  private Matrix matrix = new Matrix();
+    //  private float scale = 1f;
+    private ScaleGestureDetector SGD;
+
+    LayoutInflater inflater;
+
+
+    private ScaleGestureDetector mScaleDetector;
+
+
+    private Matrix matrix = new Matrix();
+    private float scale = 1f;
+    private ScaleGestureDetector scaleGestureDetector;
+    SubsamplingScaleImageView imageView;
+    Toast t1, t2;
+    int pagecount;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,21 +73,27 @@ public class PDFRenderFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Retain view references.
-        image = (ImageView) view.findViewById(R.id.image);
+        //  image = (ImageView) view.findViewById(R.id.image);
         btnPrevious = (EditText) view.findViewById(R.id.edittext);
         btnNext = (Button) view.findViewById(R.id.btn_next);
         zoom = (ZoomControls) view.findViewById(R.id.zoomControls);
 
 
-       // btn_previous_top = (Button) view.findViewById(R.id.btn_previous);
-       // btn_nxt_top = (Button) view.findViewById(R.id.btn_next1);
+        btn_previous_top = (Button) view.findViewById(R.id.btn_previous);
+        btn_nxt_top = (Button) view.findViewById(R.id.btn_next1);
+
 
         //set buttons event
-        //  btn_previous_top.setOnClickListener(onActionListener_click(-1)); //previous button clicked
-        // btn_nxt_top.setOnClickListener(onActionListener_click(1));
+        btn_nxt_top.setOnClickListener(onActionListener_click_next(1)); //previous button clicked
+        btn_previous_top.setOnClickListener(onActionListener_click_next(-1));
 
 
-        btnNext.setOnClickListener(onActionListener(1)); //next button clicked
+        btnNext.setOnClickListener(onActionListener_click(1)); //next button clicked
+
+        imageView = (SubsamplingScaleImageView) view.findViewById(R.id.image);
+
+
+        // SGD = new ScaleGestureDetector(getActivity().getApplicationContext(),new ScaleListener());
 
 
         int index = 0;
@@ -66,7 +102,39 @@ public class PDFRenderFragment extends Fragment {
             index = savedInstanceState.getInt("current_page", 0);
         }
         showPage(index);
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view1 = getActivity().getCurrentFocus();
+                if (view1 != null) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInputFromInputMethod(view1.getWindowToken(), 0);
+                }
+            }
+        });
 
+
+//        mScaleDetector = new ScaleGestureDetector(getActivity().getApplicationContext(), new ScaleGestureDetector.OnScaleGestureListener() {
+//            @Override
+//            public void onScaleEnd(ScaleGestureDetector detector) {
+//            }
+//            @Override
+//            public boolean onScaleBegin(ScaleGestureDetector detector) {
+//                return true;
+//            }
+//            @Override
+//            public boolean onScale(ScaleGestureDetector detector) {
+//
+//                scale *= detector.getScaleFactor();
+//                scale = Math.max(0.1f, Math.min(scale, 5.0f));
+//
+//                matrix.setScale(scale, scale);
+//                image.setImageMatrix(matrix);
+//
+//              //  Log.d(LOG_KEY, "zoom ongoing, scale: " + detector.getScaleFactor());
+//                return false;
+//            }
+//        });
 
         zoom.setOnZoomInClickListener(new View.OnClickListener() {
 
@@ -74,11 +142,11 @@ public class PDFRenderFragment extends Fragment {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
 
-                float x = image.getScaleX();
-                float y = image.getScaleY();
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
 
-                image.setScaleX((float) (x + 0.1));
-                image.setScaleY((float) (y + 0.1));
+                imageView.setScaleX((float) (x + 0.1));
+                imageView.setScaleY((float) (y + 0.1));
             }
         });
 
@@ -89,14 +157,74 @@ public class PDFRenderFragment extends Fragment {
                 // TODO Auto-generated method stub
 
 
-                float x = image.getScaleX();
-                float y = image.getScaleY();
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
 
-                image.setScaleX((float) (x - 0.1));
-                image.setScaleY((float) (y - 0.1));
+                imageView.setScaleX((float) (x - 0.1));
+                imageView.setScaleY((float) (y - 0.1));
+
+
             }
         });
+        //scaleGestureDetector = new ScaleGestureDetector(getActivity().getApplicationContext(), new ScaleListener());
     }
+
+//    public boolean onTouchEvent(MotionEvent ev) {
+//        super.getActivity();
+//        scaleGestureDetector.onTouchEvent(ev);
+//        return true;
+//    }
+
+//    private class ScaleListener extends
+//            ScaleGestureDetector.SimpleOnScaleGestureListener {
+//        @Override
+//        public boolean onScale(ScaleGestureDetector detector) {
+//            scale *= detector.getScaleFactor();
+//            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+//            matrix.setScale(scale, scale);
+//            image.setImageMatrix(matrix);
+//            return true;
+//        }
+//    }
+
+
+//    private class ScaleListener extends ScaleGestureDetector.
+//
+//            SimpleOnScaleGestureListener {
+//        @Override
+//        public boolean onScale(ScaleGestureDetector detector) {
+//            scale *= detector.getScaleFactor();
+//            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+//
+//            matrix.setScale(scale, scale);
+//            image.setImageMatrix(matrix);
+//            return true;
+//        }
+//    }
+
+
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
     @Override
@@ -176,7 +304,9 @@ public class PDFRenderFragment extends Fragment {
         // Here, we render the page onto the Bitmap.
         currentPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
         // showing bitmap to an imageview
-        image.setImageBitmap(bitmap);
+        imageView.setImage(ImageSource.bitmap(bitmap));
+
+        //  image.setImageBitmap(bitmap);
         updateUIData();
     }
 
@@ -185,13 +315,14 @@ public class PDFRenderFragment extends Fragment {
      */
     private void updateUIData() {
         int index = currentPage.getIndex();
-        int pageCount = pdfRenderer.getPageCount();
-        //  btnPrevious.setEnabled(0 != index);
-        //  btnNext.setEnabled(index + 1 < pageCount);
-        getActivity().setTitle(getString(R.string.app_name, index + 1, pageCount));
+        pagecount = pdfRenderer.getPageCount();
+
+        btnPrevious.setEnabled(0 != index);
+        btnNext.setEnabled(index + 1 < pagecount);
+        getActivity().setTitle(getString(R.string.app_name, index + 1, pagecount));
     }
 
-    private View.OnClickListener onActionListener(final int i) {
+    private View.OnClickListener onActionListener_click(final int i) {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -199,18 +330,44 @@ public class PDFRenderFragment extends Fragment {
                 String page_No = btnPrevious.getText().toString();
 
 
-//                if (i < 0) {//go to previous page
-//                    showPage(currentPage.getIndex() - 1);
-//                }
-                //else {
                 if (page_No.equalsIgnoreCase("")) {
-                    Toast t = Toast.makeText(getActivity().getApplicationContext(), "enter page no", Toast.LENGTH_SHORT);
-                    t.show();
+                    t1 = Toast.makeText(getActivity().getApplicationContext(), " Enter Page No", Toast.LENGTH_SHORT);
+                    t1.show();
+                } else if (Integer.parseInt(page_No) > 484) {
+                    t2 = Toast.makeText(getActivity().getApplicationContext(), "Invalid Page No", Toast.LENGTH_SHORT);
+                    t2.show();
                 } else {
                     int p_n = Integer.parseInt(page_No);
                     showPage(p_n);
+                    View view = getActivity().getCurrentFocus();
+                    if (view != null) {
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    }
+
                 }
-                //}
+
+
+            }
+        };
+    }
+
+
+    private View.OnClickListener onActionListener_click_next(final int i) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (i < 0) {//go to previous page
+                    if (currentPage.getIndex() != 0) {
+
+                        showPage(currentPage.getIndex() - 1);
+                    }
+                } else {
+                    showPage(currentPage.getIndex() + 1);
+                }
+
             }
         };
     }
